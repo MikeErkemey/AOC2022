58324519da9c6cd79c2a0cbaa0da535effbeab50
Mike Erkemeij <M.J.J.S.Erkemeij@student.tudelft.nl>
2022-12-01 19:59:58 +0100

    setup

---
+++ Days/AOC.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class AOC {
+
+
+    public static List<String> input(int day) {
+
+        File file = new File("inputs/" + day + ".txt");
+        if(!file.exists())
+        {
+            return new ArrayList<>();
+        }
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<String> input = new ArrayList<>();
+
+        while(sc.hasNextLine()) {
+            input.add(sc.nextLine());
+        }
+
+        return input;
+    }
+
+}
--- Days/Day1.java
+++ Days/Day1.java
@@ -1,2 +1,10 @@
+import java.util.*;
+
 public class Day1 {
+
+    public static void main(String[] args) {
+        List<String> list = AOC.input(1);
+        
+
+    }
 }
