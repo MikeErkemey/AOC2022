8e1bcf7a69bde4f25a01f1469a0776652833fb67
Mike Erkemeij <M.J.J.S.Erkemeij@student.tudelft.nl>
2022-12-02 08:22:06 +0100

    day 2

---
+++ Days/Day2.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("part1: " + solution(AOC.input(2), 1));
+        System.out.println("part2: " + solution(AOC.input(2), 2));
+    }
+
+    public static int solution(List<String> input, int part) {
+        int points = 0;
+
+        for (String s : input) {
+            int choice = s.toLowerCase().charAt(2) - 'x' + 1;
+            int opponentChoice = s.toLowerCase().charAt(0) - 'a' + 1;
+
+            if(part == 1)
+                points += choice + (choice == opponentChoice ? 3 : ((choice - 1) == opponentChoice % 3 ? 6 : 0));
+            if(part == 2)
+                points += choice == 1 ? (opponentChoice + 1) % 3 + 1  : choice == 2 ? 3 + opponentChoice  :  6 + (opponentChoice) % 3 + 1;
+        }
+        
+        return points;
+    }
+}
