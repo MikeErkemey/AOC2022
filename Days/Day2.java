82edc7ba587ecc9a03002bd69ebf2d2ab3c98d5b
Mike Erkemeij <M.J.J.S.Erkemeij@student.tudelft.nl>
2022-12-02 16:23:24 +0100

    day2

--- Days/Day2.java
+++ Days/Day2.java
@@ -11,13 +11,13 @@ public class Day2 {
         int points = 0;
 
         for (String s : input) {
-            int choice = s.toLowerCase().charAt(2) - 'x' + 1;
-            int opponentChoice = s.toLowerCase().charAt(0) - 'a' + 1;
+            int choice = s.charAt(2) - 'X' + 1;
+            int opponentChoice = s.charAt(0) - 'A' + 1;
 
             if(part == 1)
-                points += choice + (choice == opponentChoice ? 3 : ((choice - 1) == opponentChoice % 3 ? 6 : 0));
+                points += choice + (choice == opponentChoice ? 3 : choice - 1 == opponentChoice % 3 ? 6 : 0);
             if(part == 2)
-                points += choice == 1 ? (opponentChoice + 1) % 3 + 1  : choice == 2 ? 3 + opponentChoice  :  6 + (opponentChoice) % 3 + 1;
+                points += choice == 1 ? (opponentChoice + 1) % 3 + 1  : choice == 2 ? 3 + opponentChoice  :  6 + opponentChoice % 3 + 1;
         }
 
         return points;
