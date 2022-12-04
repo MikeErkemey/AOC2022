d7a710ce7fd5705ce293cd86a6c4b8cf9ea22aa7
Mike Erkemeij <M.J.J.S.Erkemeij@student.tudelft.nl>
2022-12-04 17:45:51 +0100

    add empty day5 for testing

---
+++ Days/Day05.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Day05 {
+
+    public static void main(String[] args) {
+        System.out.println("part1: " + solution(AOC.input(5), 1));
+        System.out.println("part2: " + solution(AOC.input(5), 2));
+    }
+
+    public static long solution(List<String> input, int part) {
+        return -1;
+    }
+
+
+}
